2b400b09744aa55a641261557664e65010120775
mcar03 <mcar03.474@gmail.com>
2025-01-14 20:33:27 +0100

    terminado version 1.4 drawer

---
+++ app/src/main/java/com/example/motosproyecto/fragment/LogoutFragment.kt
@@ -0,0 +1,47 @@
+package com.example.motosproyecto.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.motosproyecto.LoginActivity
+import com.example.motosproyecto.MainActivity
+import com.example.motosproyecto.R
+import com.google.firebase.auth.FirebaseAuth
+
+class LogoutFragment : Fragment() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = FirebaseAuth.getInstance()  // Inicializa FirebaseAuth
+        initEvent()
+    }
+
+    private fun initEvent() {
+        logout()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_logout, container, false)
+    }
+
+    private fun logout() {
+        // Cerrar sesión con Firebase
+        auth.signOut()
+
+        // Redirigir al usuario a la pantalla de inicio de sesión
+        val intent = Intent(activity, LoginActivity::class.java)
+        startActivity(intent)
+
+        // Finalizar la actividad para que no vuelva a la pantalla de ajustes
+        activity?.finish()
+    }
+}
